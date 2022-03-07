f4312574fd8b5af42084b136d8e6484b6638dabb
DATACOM-NZ\Jayson.Faderanga <jayson.faderanga@datacom.co.nz>
2022-03-07 14:51:07 +1300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pmd
-Quick salesforce and apex class code analyser
+# Quick salesforce and apex class code analyser
+
+## Pre-requisites
+1. Download and install PMD Source Code Analyzer from https://pmd.github.io/
+2. If you're on windows just use git bash to run the script
+
+## Instructions
+1. Copy the pmd.sh in the root of your sfdx project
+2. Execute the command for either apex class or visualforce code
+```
+sh pmd.sh -t apexclasses -p "./force-app/main/default/classes" -o html -r "./output.html"
+```
+You can also shortcut the path for apex like below.
+```
+sh pmd.sh -t apexclasses -p classes -o html -r "./output.html"
+sh pmd.sh -t apexpages -p pages -o html -r "./output.html"
+```
+
+## Parameters
+`-t` type - apexclasses or apexpages <br>
+`-p` path of files to analyze, can be a full path or shortcut for sfdx classes/pages <br>
+`-o` the output file format, mostly html, check the pmd docs for more format information <br>
+`-r` the output directory, default to pmdoutput.html if output and directory is not provided <br>
---
+++ pmd.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+while getopts t:p:o:r: flag
+do
+    case "${flag}" in
+        t) type=${OPTARG};;
+        p) path=${OPTARG};;
+        o) output=${OPTARG};;
+        r) reportfile=${OPTARG};;
+    esac
+done
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo $type
+echo $path
+
+if [ "$type" = "apexclasses" ]; then
+    ruleset="category/apex/design.xml";
+elif [ "$type" = "apexpages" ]; then
+    ruleset="category/vf/security.xml/VfUnescapeEl";
+else
+    ruleset=""
+fi
+
+if [ "$path" = "classes" ]; then
+    path="./force-app/main/default/classes";
+elif [ "$path" = "pages" ]; then
+    path="./force-app/main/default/pages";
+fi
+
+if [ -z "$reportfile" ] && [ -z "$output" ]; then
+    reportfile="./pmdoutput.html"
+elif [ -z "$reportfile" ]; then
+    reportfile="./pmdoutput.${output}"   
+fi
+
+if [ "$type" != "apexclasses" ] && [ "$type" != "apexpages" ]; then
+    printf "${RED}ERROR!${NC} valid types are apexclasses or apexpages"
+    exit 0
+elif [ -z "$type" ]; then
+    printf "${RED}Missing!${NC} type -t"
+    exit 0
+elif [ -z "$path" ]; then
+    printf "${RED}Missing!${NC} path -p"
+    exit 0
+elif [ -z "$reportfile" ]; then
+    printf "${RED}Missing!${NC} output directory -r"
+    exit 0
+else
+    pmd.bat -d "$path" -f "$output" -R "$ruleset" --report-file "$reportfile" --no-cache
+fi
